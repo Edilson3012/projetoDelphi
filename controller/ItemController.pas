31a07e8e81fbeafb941ae4cf970ee0c84defa334
Edilson Murbach <edilsongotardi@live.com>
2018-11-19 21:47:39 -0200

    Alterações no ItemController

--- controller/ItemController.pas
+++ controller/ItemController.pas
@@ -2,6 +2,47 @@ unit ItemController;
 
 interface
 
+uses
+  Conexao, System.SysUtils;
+
+type
+  TItemController = class
+  private
+    zConexao = TfrmConexao;
+    class var FInstance : TItemController;
+    
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    class function GetInstance : TItemController;
+
+    property Conexao : TfrmConexao read zConexao write zConexao;
+
+  end;
+
 implementation
+
+{ TItemController }
+
+constructor TItemController.Create;
+begin
+  zConexao = TfrmConexao.Create;
+end;
+
+destructor TItemController.Destroy;
+begin
+  FreeAndNil(zConexao);
+  inherited;
+end;
+
+class function TItemController.GetInstance: TItemController;
+begin
+  if not Assigned(self.fInstance) then
+  begin
+    self.fInstance := TItemController.Create();
+  end;
+  Result := self.fInstance;
+end;
 
 end.
